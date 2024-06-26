19a96f5e23d3a6fe6729495cba9782966415fdbf
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2024-06-26 16:03:15 +0000

    Added HomeTransaction to my screen

--- my-project/App.js
+++ my-project/App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './Screens/HomeScreen';
 import SettingsScreen from './Screens/SettingsScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='Home' component={HomeScreen} />
+          <Stack.Screen name='Settings' component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -17,8 +25,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FAFAFD',
   },
 });
