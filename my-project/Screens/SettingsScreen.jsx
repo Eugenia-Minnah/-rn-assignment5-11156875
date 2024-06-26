e75764225aa10409c966f997ed03105e99889aae
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2024-06-26 20:35:40 +0000

    Added Theme to my settings screen

--- my-project/Screens/SettingsScreen.jsx
+++ my-project/Screens/SettingsScreen.jsx
@@ -1,6 +1,11 @@
-import { View, ScrollView, Text ,StyleSheet, Pressable, Image} from 'react-native'
-import React from 'react'
+import { View, ScrollView, Text ,StyleSheet, Pressable, Image, Switch} from 'react-native'
+import React, { useState } from 'react';
+
 export default function SettingsScreen() {
+  const [themeEnabled, setThemeEnabled] = useState(false);
+  const toggleTheme = () => {
+    setThemeEnabled(!themeEnabled);
+  };
   const settingsOptions =[
     {id:'1', title:'Language' },
     {id:'2', title:'My Profile' },
@@ -19,6 +24,17 @@ export default function SettingsScreen() {
       <Image source={require('./../assets/right_arrow.png')} style={styles.arrow}/>
       </View>
     ))}
+    <View style={styles.themeContainer}>
+        <Text style={styles.themeText}>Theme</Text>
+        <Switch
+          value={themeEnabled}
+          onValueChange={toggleTheme}
+          trackColor={{ false: 'grey', true: 'white' }}
+          thumbColor={themeEnabled ? 'black' : 'white'}
+          ios_backgroundColor=""
+          style={styles.switch}
+        />
+      </View>
     </ScrollView>
   );
 }
@@ -56,5 +72,22 @@ const styles = StyleSheet.create({
     height:26,
     width:26,
     top:30,
-  }
+    color: '#0000FF'
+  },
+  themeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 120,
+    paddingHorizontal: 10,
+  },
+  themeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontWeight: '400',
+    left: 30
+  },
+  switch: {
+    marginLeft: 10,
+  },
 });
